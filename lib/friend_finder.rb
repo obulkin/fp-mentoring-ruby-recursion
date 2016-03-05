e010547aa7767930b9b851eed0fb9d91ab081334
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-05 02:18:01 +0000

    Satisfied test 1

--- lib/friend_finder.rb
+++ lib/friend_finder.rb
@@ -1,17 +1,28 @@
 require "net/http"
 require "json"
 require "uri"
+require "awesome_print"
 
 class FriendFinder
-
   def initialize(url)
     @uri = URI(url)
   end
 
   def find_by(name)
+    friend_tree = response
+    @name = name
+    find_helper(friend_tree["friends"]) 
+  end
+
+  def find_helper(friend_tree)
+    friend_tree.each do |friend|
+      return friend if friend["name"] == @name
+      return find_helper(friend["friends"]) unless friend["friends"].empty?
+    end
   end
 
   def number_of_friends
+    ap response
   end
 
   def number_of_friends_who_work_at(company_name)
@@ -22,7 +33,6 @@ class FriendFinder
   end
 
   private
-
   def raw_response
     @raw_response ||= Net::HTTP.get(@uri)
   end
@@ -30,5 +40,4 @@ class FriendFinder
   def response
     JSON.parse(raw_response)
   end
-
 end
