c03fd7e40be71d4c6b6e4a76eabe71a4bcd66f66
Oleg Bulkin <o.bulkin@gmail.com>
2016-03-12 01:51:00 +0000

    Satisfied tests 2 and 3

--- lib/friend_finder.rb
+++ lib/friend_finder.rb
@@ -22,10 +22,30 @@ class FriendFinder
   end
 
   def number_of_friends
-    ap response
+    @count = 0
+    number_of_friends_helper(response["friends"])
+    @count    
+  end
+
+  def number_of_friends_helper(friend_tree)
+    friend_tree.each do |friend|
+      @count += 1
+      number_of_friends_helper(friend["friends"]) unless friend["friends"].empty?
+    end
   end
 
   def number_of_friends_who_work_at(company_name)
+    ap response
+    count = 0
+    return number_of_friends_who_work_at_helper(response["friends"], company_name, count)  
+  end
+
+  def number_of_friends_who_work_at_helper(friend_tree, company_name, count)
+    friend_tree.each do |friend|
+      count += 1 if friend["company_name"] == company_name
+      count = number_of_friends_who_work_at_helper(friend["friends"], company_name, count) unless friend["friends"].empty?
+    end
+    return count
   end
 
   def meta
